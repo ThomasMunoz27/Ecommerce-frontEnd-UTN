ea796a095b1d47d47fcb166a82d43b4e11010420
maurogarzia <maurogarzia2@gmail.com>
2025-05-01 15:31:37 -0300

    Subo ModalRegister

--- src/components/screens/MainScreen/MainScreen.tsx
+++ src/components/screens/MainScreen/MainScreen.tsx
@@ -1,12 +1,15 @@
+
 import { Footer } from "../../ui/footer/Footer"
 import { Header } from "../../ui/Header/Header"
+import { AccountModal } from "../../ui/Modals/AccountRegisterModal/AccountModal"
+
 
 export const MainScreen = () => {
   return (
     <>
         <Header/>
     
-    
+        <AccountModal/>
     
         <Footer/>
     </>
