7a5f79727f433f78a8524c318fe0599288fb8fb7
erredev-JS <roddev100@gmail.com>
2025-05-01 13:53:22 -0300

    Header?

--- src/components/screens/MainScreen/MainScreen.tsx
+++ src/components/screens/MainScreen/MainScreen.tsx
@@ -1,13 +1,14 @@
 import { Footer } from "../../ui/footer/Footer"
+import { Header } from "../../ui/Header/Header"
 
 export const MainScreen = () => {
   return (
     <>
+        <Header/>
     
     
     
-    
-        <Footer></Footer>
+        <Footer/>
     </>
   )
 }
--- src/components/ui/Header/Header.tsx
+++ src/components/ui/Header/Header.tsx
@@ -1,6 +1,37 @@
 import style from './Header.module.css'
 export const Header = () => {
   return (
-    <Header></Header>
+    <>
+    <div className={style.headerContainer}>
+
+    <header className={style.mainHeader}>
+    <img src="./img/Logo.png" alt="" className={style.headerLogo} />
+    <nav>
+      <a href="">Inicio</a>
+      <a href="">Productos</a>
+    </nav>
+    <div className={style.searchBarIconsContainer}>
+      
+    <input type="text" placeholder='Buscar' className={style.searchBar}/>
+
+    <a href=""><img src="./icons/userCircle.svg" alt="" /></a>
+    <a href=""><img src="./icons/cartIcon.svg" alt="" /></a>
+    </div>
+
+
+    </header>
+    <div className={style.subHeader}>
+      <nav>
+        <a href="">Niños</a>
+        <a href="">Deportes</a>
+        <a href="">Calzado</a>
+        <a href="">Indumentaria</a>
+        <a href="">Hombre</a>
+        <a href="">Mujer</a>
+        <a href="">Ofertas</a>
+      </nav>
+    </div>
+    </div>
+    </>
   );
 };
