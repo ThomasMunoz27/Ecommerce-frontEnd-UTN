5ff0108e8400167021b8f04e303905fb73fa6fa2
maurogarzia <maurogarzia2@gmail.com>
2025-05-06 09:58:26 -0300

    Cambio el valor de valueLogin de modalAccount

--- src/store/useStoreModal.ts
+++ src/store/useStoreModal.ts
@@ -1,19 +1,19 @@
 import { create } from "zustand";
 
 interface IStoreModal{
-    modalAccount : boolean,
+    modalAccount : {type: boolean, valueLogin: boolean},
     modalEditLogin : {type : boolean, option : 1 | 2 | null}
-    openModalAccount : VoidFunction,
+    openModalAccount : (selection : boolean) => void,
     closeModalAccount : VoidFunction,
     openModalEditLogin : (selection : 1 | 2 | null) => void
     closeModalEditLogin : VoidFunction
 }
 
 export const useStoreModal = create<IStoreModal>((set) => ({
-    modalAccount : false,
+    modalAccount : {type : false, valueLogin : false},
     modalEditLogin : {type : false, option : null},
-    openModalAccount : () =>set({modalAccount : true}),
-    closeModalAccount : () => set({modalAccount : false}),
+    openModalAccount : (selection) =>set({modalAccount : {type: true, valueLogin : selection}}),
+    closeModalAccount : () => set({modalAccount : {type : false, valueLogin: false}}),
     openModalEditLogin : (selection) => set({modalEditLogin : {type : true, option : selection} }),
     closeModalEditLogin : () => set({modalEditLogin : {type : false, option : null}})
 
