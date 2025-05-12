aca29f894bed7f1edcb540c75e98eac35cd7164b
maurogarzia <maurogarzia2@gmail.com>
2025-05-12 17:47:52 -0300

    I do CRUD of provisional user

--- src/cruds/crudUsers.ts
+++ src/cruds/crudUsers.ts
@@ -9,6 +9,46 @@ export const getAllUsers = async (): Promise<IUser[]> => {
         const response = await axios.get(URL_USERS)
         return response.data
     }catch (err){
+        console.log("Error en getAllUsers" + err)
+        return []
+    }
+}
+
+export const getUserById = async (idUser : number): Promise<IUser[]> => {
+    try{
+        const response = await axios.get(`${BASE_URL}/${idUser}`)
+        return response.data
+    }catch (err){
+        console.log("Error en getUserById" + err)
+        return []
+    }
+}
+
+export const createUser = async (newUser : IUser): Promise<IUser[]> => {
+    try{
+        const response = await axios.post(BASE_URL, newUser)
+        return response.data
+    }catch (err){
+        console.log("Error en createUser" + err)
+        return []
+    }
+}
+
+export const updateUser = async ( userUpdated : IUser): Promise<IUser[]> => {
+    try{
+        const response = await axios.put(`${BASE_URL}/${userUpdated.id}`, userUpdated)
+        return response.data
+    }catch (err){
+        console.log("Error en updateUser" + err)
+        return []
+    }
+}
+
+export const deleteUser = async (idUser : number): Promise<IUser[]> => {
+    try{
+        const response = await axios.delete(`${URL_USERS}/${idUser}`)
+        return response.data
+    }catch (err){
         console.log("Error en getAllCategories" + err)
         return []
     }
