19ffd98c75161f64963273029c6dc63641550fb2
maurogarzia <maurogarzia2@gmail.com>
2025-05-16 10:19:19 -0300

    I modify the color handle in the SubModalAdmin

--- src/components/ui/Modals/SubModalAdmin/SubModalAdmin.tsx
+++ src/components/ui/Modals/SubModalAdmin/SubModalAdmin.tsx
@@ -1,15 +1,17 @@
-import { useEffect, useState } from 'react'
+import React, { useEffect, useState } from 'react'
 import { useStoreModal } from '../../../../store/useStoreModal'
 import useStoreProduct from '../../../../store/useStoreProduct'
 import styles from './SubModalAdmin.module.css'
 import { getAllCategories } from '../../../../cruds/crudCategory'
 import { ICategory } from '../../../../types/ICategory'
+import { IColor } from '../../../../types/IColor'
 
 export const SubModalAdmin = () => {
 
     const {closeOpenModalSubAdmin, modalSubAdmin} = useStoreModal()
     const {activeProduct} = useStoreProduct()
     const [categories, setCategories] = useState<ICategory[]>()
+    const [color, setColor] = useState<string>("#000000") // Valor negro por defecto
     
     useEffect(() => {
         const obtainCategories = async() => {
@@ -19,6 +21,30 @@ export const SubModalAdmin = () => {
 
         obtainCategories()
     },[])
+
+    // Manejo el cambio de estado de los colores
+    const handleChangeColor = (e : React.ChangeEvent<HTMLInputElement>) => {
+        setColor(e.target.value)
+    }
+
+    const handleSubmitColors = () => {
+        if (!activeProduct) return null
+
+        const existingColor = activeProduct.colors.some(c => c.value === color)
+        if (existingColor) {
+            alert('Color ya agregado')
+            return
+        }
+
+        const maxId = Math.max(...activeProduct.colors.map(c => c.id), 0)
+        const newColor: IColor = {
+            id : maxId + 1,
+            value: color
+        }
+
+        activeProduct.colors.push(newColor)
+
+    }
 
 
 
@@ -53,9 +79,21 @@ export const SubModalAdmin = () => {
     }else if(modalSubAdmin.option === 2){
         return(
             <div className={styles.containerPrincipal}>
-                Modal de colores
+                <div className={styles.containerColor}>
+                    <h3>Colores del producto: {activeProduct?.name}</h3>
+                    <select name="" id="">
+                        {activeProduct?.colors.map(color => (
+                            <option key={color.id} value=''>{color.value}</option>
+                        ))}
+                    </select>
+                </div>
+                <div className={styles.containerColor}>
+                    <h3>Agregar Color</h3>
+                        <input type="color" value={color} name="" id="" placeholder='Elegir color' onChange={handleChangeColor}/>
+                        <p>{color}</p>
+                </div>
                 <div className={styles.containerButtons}>
-                    <button>Aceptar</button>
+                    <button onClick={handleSubmitColors}>Aceptar</button>
                     <button onClick={closeOpenModalSubAdmin}>Cancelar</button>
                 </div>
             </div>
