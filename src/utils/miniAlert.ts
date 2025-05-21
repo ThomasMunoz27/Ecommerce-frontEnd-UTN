b10b8ceef36d8dbe4f1b656d62d456a78f84d84b
ThomasMunoz27 <thomasnm2004@gmail.com>
2025-05-21 20:23:05 -0300

    miniAlert generic

---
+++ src/utils/miniAlert.ts
@@ -0,0 +1,24 @@
+import Swal from "sweetalert2";
+
+//Popup pequeño que sale abajo a la derecha de la pantalla, recibe un Titulo y un Subtitulo opcional
+export const miniAlert = (title: String, subtitle: String | "") => {
+    let timerInterval = 4000;
+    Swal.fire({
+        title: title,
+        html: subtitle,
+        timer: 4000,
+        timerProgressBar: true,
+        icon: "success",
+        toast: true, // ✅ Estilo tipo toast (más pequeño y sin fondo gris)
+        position: 'bottom-end', // ✅ Abajo a la derecha
+        showConfirmButton: false, // ❌ Sin botón de confirmación
+        background: "#fff", // Fondo blanco, o el que prefieras
+        didOpen: () => {
+        Swal.showLoading();
+        
+        },
+        willClose: () => {
+        clearInterval(timerInterval);
+        }
+    });
+}
