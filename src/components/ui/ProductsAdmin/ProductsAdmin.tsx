99bdb70b7b68bd10fb1d49e59e07480695dbde7d
ThomasMunoz27 <thomasnm2004@gmail.com>
2025-05-26 09:06:48 -0300

    editProduct in productsAdmin fix

--- src/components/ui/ProductsAdmin/ProductsAdmin.tsx
+++ src/components/ui/ProductsAdmin/ProductsAdmin.tsx
@@ -15,12 +15,11 @@ export const ProductsAdmin = () => {
 
     useEffect(() => {
         const getProducts = async () => {
-        const products = await getAllProducts();
-            console.log(products);
-            setProducts(products);
+        const productsFetched = await getAllProducts();
+            setProducts(productsFetched);
         };
     getProducts();
-    }, []);
+    }, [products]);
 
     // Pongo el producto a editar como producto activo
     const handleEdit = (product : IProduct) => {
