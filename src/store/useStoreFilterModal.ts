16b7bd21ae18c08feab2d11da78a1a4ca615f61f
erredev-JS <rodrigozapata968@gmail.com>
2025-05-29 13:41:35 -0300

    Estilos filter modal0

--- src/store/useStoreFilterModal.ts
+++ src/store/useStoreFilterModal.ts
@@ -1,11 +1,49 @@
 import { create } from "zustand";
 
 interface IUseStoreFilterModal {
+
+    // Visible
     visible : boolean
     toggleVisible : () => void
+
+    // Drop orderBy
+
+    orderByDropped : boolean
+    toggleOrderBy : () => void
+
+    // Drop sex
+
+    sexDropped : boolean
+    toggleSex : () => void
+
+    // Drop color
+
+    colorDropped : boolean
+    toggleColor : () => void
+
+
 }
 
 export const useStoreFilterModal = create<IUseStoreFilterModal>((set) => ({
+
+    // Visible
+
     visible : false,
-    toggleVisible : () => set((state) => ({visible : !state.visible}))
+    toggleVisible : () => set((state) => ({visible : !state.visible})),
+
+    // Order by
+
+    orderByDropped: false,
+    toggleOrderBy : () => set((state) => ({orderByDropped: !state.orderByDropped})),
+
+    // Drop sex
+
+    sexDropped: false,
+    toggleSex : () => set((state) => ({sexDropped: !state.sexDropped})),
+
+    // Drop color
+
+    colorDropped: false,
+    toggleColor : () => set((state) => ({colorDropped: !state.colorDropped}))
+
 }))
