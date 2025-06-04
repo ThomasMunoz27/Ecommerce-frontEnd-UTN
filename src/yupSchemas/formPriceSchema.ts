7ea8ff95520f99643472ed0385de2c2ffce6ae3e
ThomasMunoz27 <thomasnm2004@gmail.com>
2025-06-04 20:12:12 -0300

    yup in prices

--- src/yupSchemas/formPriceSchema.ts
+++ src/yupSchemas/formPriceSchema.ts
@@ -1,8 +1,20 @@
 import { number, object } from "yup" 
 
 export const formPriceSchema = object({
-    purchasePrice : number().required("Campo obligatorio").min(0),
-    salePrice : number().required("Campo obligatorio").min(0),
-        
-
+	purchasePrice: number()
+		.required("Campo obligatorio")
+		.min(1, "Debe ser mayor o igual a 1"),
+	salePrice: number()
+		.required("Campo obligatorio")
+		.min(2, "Debe ser mayor o igual a 2")
+		.test(
+			"greater-than-purchase",
+			"El precio de venta debe ser mayor al precio de compra",
+			function (value) {
+				const { purchasePrice } = this.parent
+				return value !== undefined && purchasePrice !== undefined
+					? value > purchasePrice
+					: true
+			}
+		),
 })
