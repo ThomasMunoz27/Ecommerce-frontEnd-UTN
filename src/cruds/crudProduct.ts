ea65d3ec4b088773f7b95ccf6b2225ac9f7bbb56
erredev-JS <rodrigozapata968@gmail.com>
2025-06-05 11:56:39 -0300

    Precios terminado

--- src/cruds/crudProduct.ts
+++ src/cruds/crudProduct.ts
@@ -1,13 +1,11 @@
-import axios from "axios"
+
 import interceptorApiClient from "../interceptors/axios.interceptor"
-import { BASE_URL } from "../utils/constantes"
 import { IProduct } from "../types/IProduct"
 import { ICreateProduct } from "../types/ICreatedProducts"
 
 
 
 
-const URL_PRODUCTS = `${BASE_URL}/api/product`
 
 export const getAllProducts = async (): Promise<IProduct[]> => {
         const response = await interceptorApiClient.get("/product")
@@ -28,22 +26,22 @@ export const postProduct = async (newProduct: ICreateProduct) => {
 }
 
 export const putProduct = async (updatedProduct: IProduct) => {
-        const response = await interceptorApiClient.post(`product/${updatedProduct.id}`, updatedProduct)
+        const response = await interceptorApiClient.post(`/product/${updatedProduct.id}`, updatedProduct)
         return response.data
 }
 
 export const deleteProduct = async (idProductToDelete: number) => {
-        const response = await interceptorApiClient.delete(`product/${idProductToDelete}`)
+        const response = await interceptorApiClient.delete(`/product/${idProductToDelete}`)
         return response.data
 }
 
 export const getAllProductsActive = async () => {
-        const response = await interceptorApiClient.get(`product/active`)
+        const response = await interceptorApiClient.get(`/product/active`)
         return response.data
 }
 
 export const getAllProductsInactive = async () => {
-    const response = await interceptorApiClient.get(`product/inactive`)
+    const response = await interceptorApiClient.get(`/product/inactive`)
     return response.data
 }
 
@@ -51,11 +49,11 @@ export const getAllProductsInactive = async () => {
 export const getAllProductsPaged = async (page: number, size: number, categoryId? : number) => {
     try {
         if(categoryId){
-            const response = await interceptorApiClient.get(`product/paged?page=${page}&size=${size}&categoryId=${categoryId}`)
+            const response = await interceptorApiClient.get(`/product/paged?page=${page}&size=${size}&categoryId=${categoryId}`)
             return response.data
 
         }else{
-            const response = await interceptorApiClient.get(`product/paged?page=${page}&size=${size}`)
+            const response = await interceptorApiClient.get(`/product/paged?page=${page}&size=${size}`)
             return response.data
         }
     } catch (error) {
@@ -65,7 +63,7 @@ export const getAllProductsPaged = async (page: number, size: number, categoryId
 }
 
 export const getProductsFiltered = async (category: string) => {
-        const response = await interceptorApiClient.get(`product/filter?categoria=${category}`)
+        const response = await interceptorApiClient.get(`/product/filter?categoria=${category}`)
         return response.data
    
 }
