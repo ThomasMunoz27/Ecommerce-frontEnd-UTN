bbd3156cdb07ee15d2f419f5a3174eecaf9f4dca
ThomasMunoz27 <thomasnm2004@gmail.com>
2025-06-05 12:38:21 -0300

    country

--- src/cruds/crudCountry.ts
+++ src/cruds/crudCountry.ts
@@ -1,56 +1,32 @@
-import axios from "axios"
-import { BASE_URL } from "../utils/constantes"
 import { ICountry } from "../types/ICountry"
+import interceptorApiClient from "../interceptors/axios.interceptor"
 
 
-const URL_COUNTRY = `${BASE_URL}/api/country`
 
 
 export const getAllCountries = async (): Promise<ICountry[]> => {
-    try{
-        const response = await axios.get(URL_COUNTRY)
-        return response.data
-    }catch (err){
-        console.log("Error en getAllCountries" + err)
-        return []
-    }
+    const response = await interceptorApiClient.get(`/country`)
+    return response.data
 }
 
 export const getCountryById = async (countryId: string): Promise<ICountry | undefined> => {
-    try{
-        const response = await axios.get(`${URL_COUNTRY}/${countryId}`)
-        return response.data
-    }catch (err){
-        console.log("Error en getCategoryById" + err)
-        return undefined
-    }
+    const response = await interceptorApiClient.get(`/country/${countryId}`)
+    return response.data
 }
 
 
 export const postCountry = async (newCountry: ICountry) => {
-    try{
-        const response = await axios.post(URL_COUNTRY, newCountry)
-        return response.data
-    }catch (err){
-        console.log("Error en postCategory" + err)
-    }
+    const response = await interceptorApiClient.post(`/country`, newCountry)
+    return response.data
 }
 
 export const putCategory = async (updatedCountry: ICountry) => {
-    try{
-        const response = await axios.put(`${URL_COUNTRY}/${updatedCountry.id}`, updatedCountry)
-        return response.data
-    }catch (err){
-        console.log("Error en putCategory" + err)
-    }
+    const response = await interceptorApiClient.put(`/country/${updatedCountry.id}`, updatedCountry)
+    return response.data
 }
 
 export const deleteCategory = async (idCountryToDelete: string) => {
-    try{
-        const response = await axios.delete(`${URL_COUNTRY}/${idCountryToDelete}`)
-        return response.data
-    }catch (err){
-        console.log("Error en deleteCategory" + err)
-    }
+    const response = await interceptorApiClient.delete(`/country/${idCountryToDelete}`)
+    return response.data
 }
 
