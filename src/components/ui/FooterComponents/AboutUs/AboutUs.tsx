bfd0141a8217dcfe0e51adeeab9a07e5f8eff2ca
ThomasMunoz27 <thomasnm2004@gmail.com>
2025-06-10 22:43:23 -0300

    footerComponents

---
+++ src/components/ui/FooterComponents/AboutUs/AboutUs.tsx
@@ -0,0 +1,25 @@
+
+import styles from './AboutUs.module.css'
+
+export const AboutUs = () => {
+  return (
+    <div className={styles.MainContainerInfo}>
+        <h2>Nuestros Datos</h2>
+
+        <p>Nuestros Datos
+Nombre comercial: Campeones
+Razón social: Campeones.com
+CUIT: 30-71234567-8
+Teléfono de contacto: +54 9 11 2345-6789
+Correo electrónico: campeones@champions.com
+Horario de atención: Lunes a Viernes de 9:00 a 18:00 hs
+Redes sociales:
+
+Instagram: @sportfit.arg
+
+Facebook: SportFit Argentina
+
+En SportFit trabajamos cada día para brindarte indumentaria deportiva de calidad, con diseños modernos, cómodos y preparados para el alto rendimiento. Estamos comprometidos con un servicio al cliente rápido y personalizado, y con garantizar la transparencia en cada paso del proceso de compra.</p>
+    </div>
+  )
+}
